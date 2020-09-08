f5b458fbcffbd31717c77c8320fa069fe9230d6b
isabellegagnon <70918344+isabellegagnon@users.noreply.github.com>
2020-09-07 21:08:36 -0400

    Update leapInfiniteLoop.js

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,15 +1,20 @@
 var controllerOptions = {};
-var i = 0;
+//var i = 0;
 var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 
+var randx;
+var randy;
+
 Leap.loop(controllerOptions, function(frame)
     {
-        console.log(i);
-        circle(x, y, 50);
-        
-        i+=1;
+        clear();
+        randx = Math.floor(Math.random()*2) - 1;
+        randy = Math.floor(Math.random()*2) - 1;
+        circle(x + randx, y + randy, 50);
 
-    }
-);
+        //console.log(i);
+        //i+=1;
+
+    });x
 
