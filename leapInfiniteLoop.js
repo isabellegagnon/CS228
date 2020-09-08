f729f08af5a4b9545cf72a1fd4024742999b1eeb
isabellegagnon <70918344+isabellegagnon@users.noreply.github.com>
2020-09-07 20:45:10 -0400

    Update leapInfiniteLoop.js

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,10 +1,15 @@
 var controllerOptions = {};
 var i = 0;
+var x = window.innerWidth/2;
+var y = window.innerHeight/2;
 
 Leap.loop(controllerOptions, function(frame)
-{
-  console.log(i);
-  i += 1;
+    {
+        console.log(i);
+        circle(x, y, 50);
+        
+        i+=1;
 
-}
+    }
 );
+
