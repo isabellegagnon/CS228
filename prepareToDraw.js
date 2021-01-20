4d0adde8b9aa6b17ac18efe397cd750baa8d6d05
isabellegagnon <70918344+isabellegagnon@users.noreply.github.com>
2021-01-19 21:05:35 -0500

    Create prepareToDraw.js

---
+++ prepareToDraw.js
@@ -0,0 +1,62 @@
+let img;
+let img_0;
+let img_1;
+let img_2;
+let img_3;
+let img_4;
+let img_5;
+let img_6;
+let img_7;
+let img_8;
+let img_9;
+let num_0;
+let num_1;
+let num_2;
+let num_3;
+let num_4;
+let num_5;
+let num_6;
+let num_7;
+let num_8;
+let num_9;
+let img_right;
+let img_left;
+let img_up;
+let img_down;
+let img_back;
+let img_forward;
+// starting digit
+var digitToShow = 4;
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    img = loadImage('https://i.imgur.com/RYcFbpU.jpg');
+    img_0 = loadImage('https://i.imgur.com/yEYBqA6.jpg');
+    img_1 = loadImage('https://i.imgur.com/5KlyzIB.jpg');
+    img_2 = loadImage('https://i.imgur.com/cIxFpZk.jpg');
+    img_3 = loadImage('https://i.imgur.com/CT92qxW.jpg');
+    img_4 = loadImage('https://i.imgur.com/1H9G9oG.jpg');
+    img_5 = loadImage('https://i.imgur.com/B00UpQc.jpg');
+    img_6 = loadImage('https://i.imgur.com/V7Yfda7.jpg');
+    img_7 = loadImage('https://i.imgur.com/hszheE3.jpg');
+    img_8 = loadImage('https://i.imgur.com/gVOX0gA.jpg');
+    img_9 = loadImage('https://i.imgur.com/rwgYqXh.jpg');
+
+    num_0 = loadImage('https://i.imgur.com/RDzidtK.jpg');
+    num_1 = loadImage('https://i.imgur.com/RvIzBoW.jpg');
+    num_2 = loadImage('https://i.imgur.com/AaVjbMt.jpg');
+    num_3 = loadImage('https://i.imgur.com/HTaZItQ.jpg');
+    num_4 = loadImage('https://i.imgur.com/FomSkxC.jpg');
+    num_5 = loadImage('https://i.imgur.com/uQ4xqm9.jpg');
+    num_6 = loadImage('https://i.postimg.cc/vT67D9CJ/6-a4.jpg');
+    num_7 = loadImage('https://i.postimg.cc/sfBb5Lzr/7-a4.jpg');
+    num_8 = loadImage('https://i.postimg.cc/2jPMGxjB/8-a4.jpg');
+    num_9 = loadImage('https://i.imgur.com/JK4mnu3.jpg');
+
+    img_right = loadImage('https://i.imgur.com/M0rEk1R.jpg');
+    img_left = loadImage('https://i.imgur.com/byqvs4b.jpg');
+    img_up = loadImage('https://i.imgur.com/0JyL7OY.jpg');
+    img_down = loadImage('https://i.imgur.com/BIGoNqT.jpg');
+    img_back = loadImage('https://i.imgur.com/P4TqWXD.jpg');
+    img_forward = loadImage('https://i.imgur.com/GgdDVJQ.jpg');
+}
+
